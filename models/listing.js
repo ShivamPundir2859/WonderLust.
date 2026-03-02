0f7133a015c2f8fe183b51943b7bf6c36ec721cd
ShivamPundir2859 <shivampundir2859@gmail.com>
2026-03-03 00:13:20 +0530

    problem solve

--- models/listing.js
+++ models/listing.js
@@ -9,11 +9,18 @@ const listingSchema = new Schema({
     description:{
         type: String
     },
+    // image: {
+    //     type:String,
+    //     default: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=1173&auto=format&fit=crop",
+    //     set :(v) => v === "" ? "https://unsplash.com/photos/seashore-during-golden-hour-KMn4VEeEPR8" : v,
+    // },
     image: {
-        type:String,
-        default: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=1173&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set :(v) => v === "" ? "https://unsplash.com/photos/seashore-during-golden-hour-KMn4VEeEPR8" : v,
-    },
+        filename: String,
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=1173&auto=format&fit=crop",
+    }},
+    
     price: {
         type: Number
     },
